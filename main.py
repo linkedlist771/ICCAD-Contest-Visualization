a487e181bcf8690ce8b94e68d04dfa1ff792ec19
LiDing <72634327+linkedlist771@users.noreply.github.com>
2023-11-01 15:22:51 +0800

    Finishe

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils import *
 def main():
     dir_path = "data/ispd2005/adaptec1"
     nodes_data, pl_data = get_router_data(dir_path)
-    visualize_router_data(nodes_data, pl_data)
+    visualize_router_data(nodes_data, pl_data, save_figure=True)
 
 if __name__ == "__main__":
     main()
